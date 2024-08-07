61b8accfa39a81dc7c07d6e470717391dc63ae80
Javier Gonzalez <javgon2306@gmail.com>
2024-08-07 16:21:09 -0300

    add components

---
+++ counter-app/src/HelloWordApp.jsx
@@ -0,0 +1,7 @@
+export const HelloWordApp = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+};
--- counter-app/src/main.jsx
+++ counter-app/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import { HelloWordApp } from "./HelloWordApp";
+function App(){
+    return(<HelloWordApp />);
+}
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <HelloWordApp />
+    </StrictMode>
+);
