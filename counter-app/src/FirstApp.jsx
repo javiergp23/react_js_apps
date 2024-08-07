a8339073a6a19770fa9cf1f07e5f3a5cd34bf0ab
Javier Gonzalez <javgon2306@gmail.com>
2024-08-07 16:52:40 -0300

    changes_01

---
+++ counter-app/src/FirstApp.jsx
@@ -0,0 +1,14 @@
+
+const newMessage = "Hola Mundo!";
+
+export const FirstApp = () => {
+
+    return( 
+    <>
+        <h1>{newMessage   }</h1>
+        <div>Mensaje numero uno</div>
+        <div>Mensaje Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, aspernatur.</div>
+        <div>{ 1 + 1 }</div>
+    </>
+    )
+}
--- counter-app/src/main.jsx
+++ counter-app/src/main.jsx
@@ -1,12 +1,14 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { HelloWordApp } from "./HelloWordApp";
+import { FirstApp } from "./FirstApp";
+
 function App(){
     return(<HelloWordApp />);
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <HelloWordApp />
+        <FirstApp />
     </StrictMode>
 );
