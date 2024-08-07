cd7a1eba95d2ecb4ad865b4e91805dfaab0b6708
Javier Gonzalez <javgon2306@gmail.com>
2024-08-07 17:09:43 -0300

    component app

--- counter-app/src/FirstApp.jsx
+++ counter-app/src/FirstApp.jsx
@@ -1,11 +1,14 @@
 
 const newMessage = "Hola Mundo!";
+const getResult = (a, b) => {
+    return a + b; 
+};
 
 export const FirstApp = () => {
 
     return( 
     <>
-        <h1>{newMessage   }</h1>
+        <h1>{getResult(2, 5)   }</h1>
         <div>Mensaje numero uno</div>
         <div>Mensaje Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, aspernatur.</div>
         <div>{ 1 + 1 }</div>
